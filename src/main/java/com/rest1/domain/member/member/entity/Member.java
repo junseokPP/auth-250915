74619514f7f304d6ac2fef69f6abc18b1851e4f9
junseokPP <pjs981130@gmail.com>
2025-09-16 09:16:56 +0900

    2강 : 회원 도메인 도입

---
+++ src/main/java/com/rest1/domain/member/member/entity/Member.java
@@ -0,0 +1,30 @@
+package com.rest1.domain.member.member.entity;
+
+import com.rest1.domain.post.comment.entity.Comment;
+import com.rest1.global.jpa.entity.BaseEntity;
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.OneToMany;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@NoArgsConstructor
+@Getter
+@Entity
+public class Member extends BaseEntity {
+
+    private String username;
+    private String password;
+    private String nickname;
+
+    public Member(String username, String password, String nickname) {
+        this.username = username;
+        this.password = password;
+        this.nickname = nickname;
+    }
+}
--- src/main/java/com/rest1/global/initData/BaseInitData.java
+++ src/main/java/com/rest1/global/initData/BaseInitData.java
@@ -1,5 +1,7 @@
 package com.rest1.global.initData;
 
+import com.rest1.domain.member.member.entity.Member;
+import com.rest1.domain.member.member.service.MemberService;
 import com.rest1.domain.post.post.entity.Post;
 import com.rest1.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
@@ -18,6 +20,7 @@ public class BaseInitData {
     @Lazy
     private BaseInitData self;
     private final PostService postService;
+    private final MemberService memberService;
 
     @Bean
     ApplicationRunner initDataRunner() {
@@ -31,6 +34,19 @@ public class BaseInitData {
 
     @Transactional
     public void work1() {
+        if(memberService.count() > 0){
+            return;
+        }
+
+        memberService.join("system","system","시스템");
+        memberService.join("admin","admin","운영자");
+        memberService.join("user1","1234","유저1");
+        memberService.join("user2","1234","유저2");
+        memberService.join("user3","1234","유저3");
+    }
+
+    @Transactional
+    public void work2() {
         if(postService.count() > 0) {
             return;
         }
