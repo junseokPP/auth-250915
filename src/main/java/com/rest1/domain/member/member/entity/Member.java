59945b3387d601e7098b87423fcd182e95372d06
junseokPP <pjs981130@gmail.com>
2025-09-16 12:32:28 +0900

    11강 : apikey random생성 방법

--- src/main/java/com/rest1/domain/member/member/entity/Member.java
+++ src/main/java/com/rest1/domain/member/member/entity/Member.java
@@ -2,30 +2,33 @@ package com.rest1.domain.member.member.entity;
 
 import com.rest1.domain.post.comment.entity.Comment;
 import com.rest1.global.jpa.entity.BaseEntity;
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @NoArgsConstructor
 @Getter
 @Entity
 public class Member extends BaseEntity {
 
+    @Column(unique = true)
     private String username;
     private String password;
     private String nickname;
+    @Column(unique = true)
+    private String apikey;
 
     public Member(String username, String password, String nickname) {
         this.username = username;
         this.password = password;
         this.nickname = nickname;
+        this.apikey = UUID.randomUUID().toString();
     }
 
     public String getName(){
