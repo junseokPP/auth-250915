79fe6045a54293ede2264785cb0523d865b649d4
junseokPP <pjs981130@gmail.com>
2025-09-18 12:48:26 +0900

    45강 : 인증 시 Authorizaiton 헤더 뿐만 아니라 Cookie 헤더에서도 인증 정보를 찾아서 처리

--- src/main/java/com/rest1/global/rq/Rq.java
+++ src/main/java/com/rest1/global/rq/Rq.java
@@ -19,17 +19,34 @@ public class Rq {
 
     public Member getActor() {
 
+        String apiKey = null;
         String authorization = request.getHeader("Authorization");
 
-        if (authorization == null || authorization.isEmpty()) {
-            throw new ServiceException("401-1", "헤더에 인증 정보가 없습니다.");
+        if(authorization != null && !authorization.isEmpty()) {
+
+            if (!authorization.startsWith("Bearer ")) {
+                throw new ServiceException("401-2", "헤더의 인증 정보 형식이 올바르지 않습니다.");
+            }
+
+            apiKey = authorization.replace("Bearer ", "");
+
+        } else {
+
+            Cookie[] cookies = request.getCookies();
+
+            if(cookies == null) {
+                throw new ServiceException("401-1", "인증 정보가 없습니다.");
+            }
+
+            for(Cookie cookie : cookies) {
+                if(cookie.getName().equals("apiKey")) {
+                    apiKey = cookie.getValue();
+                    break;
+                }
+            }
         }
 
-        if (!authorization.startsWith("Bearer ")) {
-            throw new ServiceException("401-2", "헤더의 인증 정보 형식이 올바르지 않습니다.");
-        }
-
-        Member actor = memberService.findByApiKey(authorization.replace("Bearer ", ""))
+        Member actor = memberService.findByApiKey(apiKey)
                 .orElseThrow(() -> new ServiceException("401-3", "API 키가 올바르지 않습니다."));
 
         return actor;
@@ -43,5 +60,6 @@ public class Rq {
         cookie.setHttpOnly(true);
 
         response.addCookie(cookie);
+
     }
 }
